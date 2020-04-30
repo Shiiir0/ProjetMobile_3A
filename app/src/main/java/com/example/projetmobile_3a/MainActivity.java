51c74352ed0cfb461eb8977906e8b44b691b53be
Quentin MICHEL <quentinmichel99@yahoo.fr>
2020-04-30 10:30:04 +0200

    Stockage des données en cache test

--- app/src/main/java/com/example/projetmobile_3a/MainActivity.java
+++ app/src/main/java/com/example/projetmobile_3a/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         /*Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);*/
 
-        //creer le stockage cache
+        //creer le stockage cache test
         SharedPreferences = getSharedPreferences("application_esiea", Context.MODE_PRIVATE);
 
         gson = new GsonBuilder()
